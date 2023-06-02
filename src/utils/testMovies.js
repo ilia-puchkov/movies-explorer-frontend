58b77f60eab6fa500aec56d840176571649d6b11
ilia-puchkov <112614894+ilia-puchkov@users.noreply.github.com>
2023-06-02 16:40:59 +0300

    Add files via upload

---
+++ src/utils/testMovies.js
@@ -0,0 +1,66 @@
+const testMovies = [
+  {
+    name: 'Incepion',
+    link: 'https://kinofilmpro.ru/wp-content/uploads/2022/02/w1500_44943050.jpg',
+    duration: '120',
+    _id: 1,
+  },
+  {
+    name: 'Braindead',
+    link: 'https://mobimg.b-cdn.net/v3/fetch/40/40d462c94e51a832909042e6057fd45f.jpeg',
+    duration: '120',
+    _id: 2,
+  },
+  {
+    name: 'Mad Max',
+    link: 'https://i.imgflip.com/2qux6n.jpg',
+    duration: '120',
+    _id: 3,
+  },
+  {
+    name: "Dune", 
+    link: 'https://a-static.besthdwallpaper.com/dune-2021-cast-characters-wallpaper-2048x1152-84579_49.jpg', 
+    duration: '220',
+    _id: 4,
+  },
+  {
+    name: "Pepa", 
+    link: 'https://www.hypresslive.com/wp-content/uploads/2017/11/Peppa-Pig-Graphic-01_.jpg.jpeg', 
+    duration: '11',
+    _id: 5,
+  },
+  {
+    name: "Wag the Dog", 
+    link: 'https://is3-ssl.mzstatic.com/image/thumb/Video113/v4/1a/f4/c3/1af4c344-54ed-5fb1-f80b-f298c992153d/pr_source.lsr/1200x675.jpg', 
+    duration: '11',
+    _id: 6,
+    isSaved: true,
+  },  
+  {
+    name: "Starship Troopers", 
+    link: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/65bd87c0-3d0c-4c14-b426-5fdf94deb3de/1920x', 
+    duration: '11',
+    _id: 7,
+    isSaved: true,
+  },
+];
+
+
+const testLikedMovies = [
+  {
+    name: "Wag the Dog", 
+    link: 'https://is3-ssl.mzstatic.com/image/thumb/Video113/v4/1a/f4/c3/1af4c344-54ed-5fb1-f80b-f298c992153d/pr_source.lsr/1200x675.jpg', 
+    duration: '11',
+    _id: 6,
+    isSaved: true,
+  },  
+  {
+    name: "Starship Troopers", 
+    link: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/65bd87c0-3d0c-4c14-b426-5fdf94deb3de/1920x', 
+    duration: '11',
+    _id: 7,
+    isSaved: true,
+  },
+];
+
+export {testMovies, testLikedMovies};
